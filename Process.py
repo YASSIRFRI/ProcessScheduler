57020894e185c0125fea523ec686a13ca00d237b
Yassir Fri <yassir.fri@um6p.ma>
2024-04-24 18:37:09 +0100

    tested file upload

--- Process.py
+++ Process.py
@@ -1,11 +1,12 @@
 class Process:
-    def __init__(self, pid=None, arrival_time=None, burst_time=None):
+    def __init__(self, pid=None, arrival_time=None, burst_time=None,priority=None):
         self.pid = pid
         self.arrival_time = arrival_time
         self.burst_time = burst_time
         self.remaining_time = burst_time
         self.waiting_time = 0
         self.turnaround_time = 0
+        self.priority = priority
 
     def __str__(self):
         return f"Process {self.pid} - Arrival Time: {self.arrival_time}, Burst Time: {self.burst_time}, Remaining Time: {self.remaining_time}, Waiting Time: {self.waiting_time}, Turnaround Time: {self.turnaround_time}"
