7b6d312255578524b350220a9995498e5c1f5d5a
Yassir Fri <yassir.fri@um6p.ma>
2024-04-27 21:33:55 +0100

    fixing colors bug

--- main.py
+++ main.py
@@ -16,13 +16,20 @@ from SchedulingAlgorithm import Priority as Priority
 from SchedulingAlgorithm import PriorityRR as PriorityRR
 from SchedulingAlgorithm import RR as RR
 import hashlib
+import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
+
+
+
+PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
 # Initialize Flask app
 app = Flask(__name__)
 
 
-dash_app = dash.Dash(__name__, server=app, url_base_pathname='/dashboard/')
+dash_app = dash.Dash(__name__, server=app, url_base_pathname='/dashboard/',external_stylesheets=[dbc.themes.BOOTSTRAP])
 dash_app.layout = html.Div(id='output')
+colors=[]
 
 
 #Shceduler should return : [proceses] [start_times] [durations]
@@ -60,34 +67,45 @@ def schedule():
             process.priority = int(priority)
     print(processes)
     scheduler.set_processes(processes)
-    #try:
     process_names,start_times,durations= scheduler.run()
     print(start_times)
     print('done')
-    #tasks = []
-    #for name, start, duration in zip(process_names, start_times, durations):
-        #start = float(start)  # Convert start to float
-        #tasks.append({'Task': name, 'Start': start, 'Finish': start + float(duration), 'Resource': name})  # Add 'Resource' key for legend
-    ## Create Gantt chart using create_gantt() function
-    #fig = ff.create_gantt(tasks, index_col='Task', title='Job Schedule', group_tasks=True, show_colorbar=True)
-    ## Customize x-axis properties to remove time units and start from 0
-    #fig.update_layout(xaxis_type='linear')
-    ## Update Dash app layout with new Gantt chart
-    #dash_app.layout = html.Div([
-        #dcc.Graph(id='job-gantt-chart', figure=fig),
-    #])
-    render(process_names, start_times, durations)
+    render(process_names, start_times, durations, arrival_times)
     return redirect(url_for('render_dashboard'))
 
 def generate_color(process_name):
-    # Generate a unique color for each process name using a hash function
     hash_object = hashlib.sha256(process_name.encode())
     hex_dig = hash_object.hexdigest()[:6]  # Take the first 6 characters of the hash as hex color code
     return '#' + hex_dig
 
+
+def render_waiting_time_chart(processes, start_times, arrival_times, durations, app_layout):
+    # Calculate turnaround times for each process
+    turnaround_times = [abs(arrival - start) + duration for start, arrival, duration in zip(start_times, arrival_times, durations)]
+    # Get unique process names and their corresponding colors
+    unique_process_names = set(processes)
+    color_dict = {process: generate_color(process) for process in unique_process_names}
+    # Generate data for bar plot using provided colors
+    data = []
+    for process, turnaround_time in zip(processes, turnaround_times):
+        color = color_dict[process]
+        data.append(go.Bar(x=[process], y=[turnaround_time], name=process, marker=dict(color=color)))
+    layout = go.Layout(
+        title='Turnaround Time for Each Process',
+        xaxis=dict(title='Process'),
+        yaxis=dict(title='Turnaround Time'),
+        barmode='group'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    app_layout.append(dcc.Graph(id='waiting-time-chart', figure=fig))
+
+    
+    
+    
 def render_gantt_chart(processes, start_times, durations, app_layout):
     tasks = []
     # Generate a custom color for each process name
+    #unique_process_names = set(processes)
     colors = [generate_color(process_name) for process_name in processes]
     for name, start, duration, color in zip(processes, start_times, durations, colors):
         start = float(start)  # Convert start to float
@@ -97,18 +115,46 @@ def render_gantt_chart(processes, start_times, durations, app_layout):
     fig.update_layout(xaxis_type='linear')
     app_layout.append(dcc.Graph(id='job-gantt-chart', figure=fig))
     
+    
+
 def add_header(app_layout):
-    header = html.H1("results", style={'color': 'blue', 'font-size': '32px', 'text-align': 'center'})
+    header = dbc.Navbar(
+        dbc.Container(
+            [
+                html.A(
+                    dbc.Row(
+                        [
+                            dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                            dbc.Col(dbc.NavbarBrand("Process Scheduling Algorithm", className="ms-2")),
+                        ],
+                        align="center",
+                        className="g-0",
+                    ),
+                    href="/",
+                    style={"textDecoration": "none"},
+                ),
+                dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+                dbc.Collapse(
+                    id="navbar-collapse",
+                    is_open=False,
+                    navbar=True,
+                ),
+            ]
+        ),
+        color="dark",
+        dark=True,
+    )
     app_layout.append(header)
 
-def add_footer( app_layout):
-    footer = html.Footer("2024", style={'text-align': 'center', 'margin-top': '20px'})
+def add_footer(app_layout):
+    footer = html.Footer("2024", className="footer text-center")
     app_layout.append(footer)
 
-def render(processes=[], start_times=[], durations=[]):
+def render(processes=[], start_times=[], durations=[], arrival_times=[]):
     app_layout = []
     add_header(app_layout)
     render_gantt_chart(processes, start_times, durations, app_layout)
+    render_waiting_time_chart(processes, start_times, arrival_times,durations, app_layout)
     add_footer(app_layout)
     dash_app.layout = html.Div(app_layout)
 
@@ -139,21 +185,10 @@ def processFile():
                     job_data.append({'pid': pid, 'arrival_time': int(arrival_time), 'burst_time': int(burst_time), 'priority': priority})
             except ValueError:
                 return "Error: Incorrect data format in CSV file."
-
-        # Handle if CSV file has trailing newline characters
     except Exception as e:
         return f"Error: {str(e)}"
-
     algorithm = request.form['algorithm']
     scheduler = Scheduler()
-
-    print("")
-    print("")
-    print("")
-    print(job_data)
-    print("")
-    print("")
-    print("")
 
     # Initialize the scheduling algorithm based on the selected algorithm
     if algorithm == 'FCFS':
@@ -182,7 +217,7 @@ def processFile():
     scheduler.set_processes(processes)
 
     process_names, start_times, durations = scheduler.run()
-    render(process_names, start_times, durations)
+    render(process_names, start_times, durations, arrival_times)
     return redirect(url_for('render_dashboard'))
 
 
